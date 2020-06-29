52d7aee9473d3baad296a5c19bcff9ccfddcddcf
Illia Halych <eli.halych@gmail.com>
2020-06-29 18:12:10 +0200

    [ADD]: create todo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,10 +3,24 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import {getUserId} from "../utils";
+import {createTodo} from "../../business-logic/todos";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  // console.log('Processing event', event);
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  const parsedBody: CreateTodoRequest = JSON.parse(event.body);
+
+  const userId = getUserId(event);
+  const newTodo = await createTodo(parsedBody, userId);
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      item: newTodo
+    })
+  }
 }

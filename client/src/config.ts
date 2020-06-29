a5dd09b9507a512fc54a2e2d68b683f5c8737de3
Illia Halych <eli.halych@gmail.com>
2020-06-29 17:48:23 +0200

    [ADD]: Auth0 configs

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,8 +3,7 @@ const apiId = '...'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-fq3tss9d.eu.auth0.com',            // Auth0 domain
+  clientId: 'u10S06Fkdw68i7NsNBuJhB6DD2Q5mrU5',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }

8533733c3d531f13f9dec2e88da14ab4ae822bab
Illia Halych <eli.halych@gmail.com>
2020-06-29 21:51:16 +0200

    [ADD]: Update Todo handler

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -7,7 +7,7 @@ import {getUserId} from "../utils";
 import {createTodo} from "../../business-logic/todos";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // console.log('Processing event', event);
+  console.log('Processing event', event);
 
   const parsedBody: CreateTodoRequest = JSON.parse(event.body);
 
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -3,11 +3,21 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import {getUserId} from "../utils";
+import {updateTodo} from "../../business-logic/todos";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  const userId = getUserId(event)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  await updateTodo(todoId, userId, updatedTodo)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({})
+  }
 }
